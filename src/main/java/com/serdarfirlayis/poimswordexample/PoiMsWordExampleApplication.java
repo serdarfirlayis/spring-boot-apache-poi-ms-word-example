59e3252a82fc51bdb09e7714b70ba3bb0f15ea5d
Serdar Raşit FIRLAYIŞ <rasit.firlayis@tubitak.gov.tr>
2022-09-19 18:33:05 +0300

    initial commit

---
+++ src/main/java/com/serdarfirlayis/poimswordexample/PoiMsWordExampleApplication.java
@@ -0,0 +1,13 @@
+package com.serdarfirlayis.poimswordexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoiMsWordExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoiMsWordExampleApplication.class, args);
+	}
+
+}
